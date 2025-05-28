21b6b8ca064a1eeb221a25f50ca022f11239220e
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-05-28 13:37:13 +0200

    Oppdater ødelagte tester

--- src/test/kotlin/MeldingTolkerTest.kt
+++ src/test/kotlin/MeldingTolkerTest.kt
@@ -7,24 +7,19 @@ import io.mockk.verify
 import no.nav.helsearbeidsgiver.dialogporten.DialogportenService
 import no.nav.helsearbeidsgiver.kafka.Melding
 import no.nav.helsearbeidsgiver.kafka.MeldingTolker
-import no.nav.helsearbeidsgiver.kafka.Sykepengesoknad
 import no.nav.helsearbeidsgiver.utils.UnleashFeatureToggles
 import no.nav.helsearbeidsgiver.utils.json.toJson
-import no.nav.helsearbeidsgiver.utils.test.wrapper.genererGyldig
-import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
-import java.util.UUID
 
 class MeldingTolkerTest :
     FunSpec({
         test("Behandle gyldig sykmelding") {
-            val orgnr = Orgnr.genererGyldig()
             val melding = sykmelding.toJson(Melding.serializer()).toString()
 
             val dialogportenServiceMock = mockk<DialogportenService>()
             val unleashFeatureTogglesMock = mockk<UnleashFeatureToggles>()
 
             every { dialogportenServiceMock.behandleSykmelding(any()) } just Runs
-            every { unleashFeatureTogglesMock.skalOppretteDialogVedMottattSykmelding(orgnr) } returns true
+            every { unleashFeatureTogglesMock.skalOppretteDialogVedMottattSykmelding(sykmelding.orgnr) } returns true
 
             val meldingTolker =
                 MeldingTolker(
@@ -38,20 +33,14 @@ class MeldingTolkerTest :
         }
 
         test("Behandle gyldig sykepengesøknad") {
-            val orgnr = Orgnr.genererGyldig()
-            val sykepengesoknad =
-                Sykepengesoknad(
-                    soknadId = UUID.randomUUID(),
-                    sykmeldingId = UUID.randomUUID(),
-                    orgnr = orgnr,
-                )
+            val sykepengesoknad = sykepengesoknad
             val melding = sykepengesoknad.toJson(Melding.serializer()).toString()
 
             val dialogportenServiceMock = mockk<DialogportenService>()
             val unleashFeatureTogglesMock = mockk<UnleashFeatureToggles>()
 
             every { dialogportenServiceMock.behandleSykepengesoknad(any()) } just Runs
-            every { unleashFeatureTogglesMock.skalOppdatereDialogVedMottattSoknad(orgnr) } returns true
+            every { unleashFeatureTogglesMock.skalOppdatereDialogVedMottattSoknad(sykepengesoknad.orgnr) } returns true
 
             val meldingTolker =
                 MeldingTolker(
