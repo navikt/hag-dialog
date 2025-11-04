9126345d6549f531a780b814073a01e2f9d9db9a
Mehdi.Zare <mehdi.zare@nav.no>
2025-11-04 12:56:50 +0100

    Db endringer

---
+++ src/main/resources/db/migration/V1_init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE dialog
+(
+    id            UUID UNIQUE NOT NULL,
+    sykmelding_id UUID UNIQUE NOT NULL,
+    opprettet     TIMESTAMP   NOT NULL DEFAULT now()
+);
+
+CREATE INDEX sykmelding_id_index ON dialog (sykmelding_id);
+
+CREATE TABLE transmission
+(
+    id                   UUID UNIQUE NOT NULL,
+    dialog_id            UUID UNIQUE NOT NULL,
+    dokument_id          UUID UNIQUE NOT NULL,
+    dokumnet_type        TEXT        NOT NULL,
+    related_transmission UUID,
+    opprettet            TIMESTAMP   NOT NULL DEFAULT now(),
+    FOREIGN KEY (dialog_id) REFERENCES dialog (id)
+);
+
