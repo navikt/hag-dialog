3e05764d71f8e1e827d816d22fbbc8988df087cf
Mehdi Zare <32900443+mettok@users.noreply.github.com>
2025-11-12 11:03:47 +0100

    Diverse endringer (#26)

--- src/main/kotlin/kafka/MeldingTolker.kt
+++ src/main/kotlin/kafka/MeldingTolker.kt
@@ -71,7 +71,7 @@ class MeldingTolker(
             }
         }.getOrElse { e ->
             sikkerLogger.error("Klarte ikke opprette/oppdatere dialog. Avbryter.", e)
-            // throw e
+            throw e
         }
     }
 
