d77f75197b6fe38cc0ce5cee2bd4a2fcd5def482
Mehdi.Zare <mehdi.zare@nav.no>
2025-11-12 13:37:38 +0100

    Utgått forespørsel

--- src/main/kotlin/dialogporten/SykepengeTransmissionRequest.kt
+++ src/main/kotlin/dialogporten/SykepengeTransmissionRequest.kt
@@ -5,9 +5,9 @@ import no.nav.helsearbeidsgiver.dialogporten.domene.Transmission
 import no.nav.helsearbeidsgiver.dialogporten.domene.TransmissionRequest
 import no.nav.helsearbeidsgiver.kafka.Inntektsmelding
 import no.nav.helsearbeidsgiver.kafka.Inntektsmeldingsforespoersel
-import no.nav.helsearbeidsgiver.kafka.UtgaattInntektsmeldingForespoersel
 import no.nav.helsearbeidsgiver.kafka.Sykepengesoeknad
 import no.nav.helsearbeidsgiver.kafka.Sykmelding
+import no.nav.helsearbeidsgiver.kafka.UtgaattInntektsmeldingForespoersel
 import java.util.UUID
 
 class SykmeldingTransmissionRequest(
