312cb2e34f968bb44309ac2462cfbd35599aa367
Magnus Sælensminde <41949947+magnusae@users.noreply.github.com>
2025-12-11 13:19:00 +0100

    Flytt testmetoder til utilsfil i testmappa (#44)
    Bakgrunn
    Vi har et knippe funksjoner i DokumentkoblingRepository.kt for å hente diverse koblinger som kun brukes i tester.
    
    Løsning
    Flytt funksjonene til en utilsfil i testmappa.

--- src/main/kotlin/database/DokumentkoblingRepository.kt
+++ src/main/kotlin/database/DokumentkoblingRepository.kt
@@ -61,11 +61,6 @@ class DokumentkoblingRepository(
             throw e
         }
 
-    fun hentSykepengesoeknad(soeknadId: UUID): SykepengesoeknadEntity? =
-        transaction(db) {
-            SykepengesoeknadEntity.findById(soeknadId)
-        }
-
     fun henteSykemeldingerMedStatusMottatt(): List<Sykmelding> =
         transaction(db) {
             SykmeldingEntity
@@ -135,20 +130,6 @@ class DokumentkoblingRepository(
             throw e
         }
 
-    fun hentListeAvSoeknadIdForVedtaksperiodeId(vedtaksperiodeId: UUID): List<UUID> =
-        transaction(db) {
-            VedtaksperiodeSoeknadEntity.find { VedtaksperiodeSoeknadTable.vedtaksperiodeId eq vedtaksperiodeId }.map {
-                it.soeknadId
-            }
-        }
-
-    fun hentSoeknaderForVedtaksperiodeId(vedtaksperiodeId: UUID): List<VedtaksperiodeSoeknadEntity> =
-        transaction(db) {
-            VedtaksperiodeSoeknadEntity
-                .find { VedtaksperiodeSoeknadTable.vedtaksperiodeId eq vedtaksperiodeId }
-                .toList()
-        }
-
     fun opprettForespoerselSendt(forespoerselSendt: ForespoerselSendt) {
         opprettForespoersel(
             forespoerselId = forespoerselSendt.forespoerselId,
@@ -184,14 +165,6 @@ class DokumentkoblingRepository(
             throw e
         }
     }
-
-    fun hentForespoerslerMedStatusMottattEldstFoerst(): List<ForespoerselEntity> =
-        transaction(db) {
-            ForespoerselEntity
-                .find { ForespoerselTable.status eq Status.MOTTATT }
-                .orderBy(ForespoerselTable.opprettet to SortOrder.ASC)
-                .toList()
-        }
 
     fun hentForespoerselSykmeldingKoblinger(): List<ForespoerselSykmeldingKobling> =
         transaction(db) {
@@ -264,12 +237,4 @@ class DokumentkoblingRepository(
             }
         }
     }
-
-    fun hentInntektsmeldingerMedStatusMottatt(): List<InntektsmeldingEntity> =
-        transaction(db) {
-            InntektsmeldingEntity
-                .find { InntektsmeldingTable.status eq Status.MOTTATT }
-                .orderBy(InntektsmeldingTable.opprettet to SortOrder.ASC)
-                .toList()
-        }
 }
---
+++ src/test/kotlin/DokumentkoblingRepositoryUtils.kt
@@ -0,0 +1,56 @@
+import dokumentkobling.Status
+import no.nav.helsearbeidsgiver.database.ForespoerselEntity
+import no.nav.helsearbeidsgiver.database.ForespoerselTable
+import no.nav.helsearbeidsgiver.database.InntektsmeldingEntity
+import no.nav.helsearbeidsgiver.database.InntektsmeldingTable
+import no.nav.helsearbeidsgiver.database.SykepengesoeknadEntity
+import no.nav.helsearbeidsgiver.database.VedtaksperiodeSoeknadEntity
+import no.nav.helsearbeidsgiver.database.VedtaksperiodeSoeknadTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
+
+fun hentSykepengesoeknad(
+    db: Database,
+    soeknadId: UUID,
+): SykepengesoeknadEntity? =
+    transaction(db) {
+        SykepengesoeknadEntity.findById(soeknadId)
+    }
+
+fun hentListeAvSoeknadIdForVedtaksperiodeId(
+    db: Database,
+    vedtaksperiodeId: UUID,
+): List<UUID> =
+    transaction(db) {
+        VedtaksperiodeSoeknadEntity.find { VedtaksperiodeSoeknadTable.vedtaksperiodeId eq vedtaksperiodeId }.map {
+            it.soeknadId
+        }
+    }
+
+fun hentSoeknaderForVedtaksperiodeId(
+    db: Database,
+    vedtaksperiodeId: UUID,
+): List<VedtaksperiodeSoeknadEntity> =
+    transaction(db) {
+        VedtaksperiodeSoeknadEntity
+            .find { VedtaksperiodeSoeknadTable.vedtaksperiodeId eq vedtaksperiodeId }
+            .toList()
+    }
+
+fun hentForespoerslerMedStatusMottattEldstFoerst(db: Database): List<ForespoerselEntity> =
+    transaction(db) {
+        ForespoerselEntity
+            .find { ForespoerselTable.status eq Status.MOTTATT }
+            .orderBy(ForespoerselTable.opprettet to SortOrder.ASC)
+            .toList()
+    }
+
+fun hentInntektsmeldingerMedStatusMottatt(db: Database): List<InntektsmeldingEntity> =
+    transaction(db) {
+        InntektsmeldingEntity
+            .find { InntektsmeldingTable.status eq Status.MOTTATT }
+            .orderBy(InntektsmeldingTable.opprettet to SortOrder.ASC)
+            .toList()
+    }
