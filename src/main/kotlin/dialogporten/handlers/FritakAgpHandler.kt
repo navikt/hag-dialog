3d4d7e6bbb84eac8e02bb45a7832bb58042986e0
Mehdi.Zare <mehdi.zare@nav.no>
2026-03-24 13:55:38 +0100

    Fritak dialogmelding pdf

--- src/main/kotlin/dialogporten/handlers/FritakAgpHandler.kt
+++ src/main/kotlin/dialogporten/handlers/FritakAgpHandler.kt
@@ -1,8 +1,11 @@
 package no.nav.helsearbeidsgiver.dialogporten.handlers
 
 import kotlinx.coroutines.runBlocking
+import no.nav.helsearbeidsgiver.Env
 import no.nav.helsearbeidsgiver.dialogporten.DialogportenClient
 import no.nav.helsearbeidsgiver.dialogporten.domene.CreateDialogRequest
+import no.nav.helsearbeidsgiver.dialogporten.domene.createApiAttachment
+import no.nav.helsearbeidsgiver.dialogporten.domene.createGuiAttachment
 import no.nav.helsearbeidsgiver.kafka.DialogMelding
 import no.nav.helsearbeidsgiver.kafka.GravidSoeknadMelding
 
@@ -30,6 +33,19 @@ class FritakAgpHandler(
                             " arbeidsgiverperioden grunnet risiko for høyt sykefravær knyttet til graviditet.",
                     transmissions = emptyList(),
                     isApiOnly = false,
+                    attachments =
+                        listOf(
+                            createApiAttachment(
+                                displayName = "sykmelding.pdf",
+                                url = "${Env.Nav.dokumentProxyBaseUrl}/v1/fritakagp/gravid/soeknad/${gravidSoeknadMelding.id}/pdf",
+                                mediaType = "application/pdf",
+                            ),
+                            createGuiAttachment(
+                                displayName = "Søknad on fritak fra arbeidsgiverperioden",
+                                url = "${Env.Nav.dokumentProxyBaseUrl}/v1/fritakagp/gravid/soeknad/${gravidSoeknadMelding.id}/pdf",
+                                mediaType = "application/pdf",
+                            ),
+                        ),
                 ),
             )
         }
