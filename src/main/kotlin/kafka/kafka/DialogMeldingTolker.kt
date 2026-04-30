01757397919423a493944a4a8e2a5576874e3368
Mehdi.Zare <mehdi.zare@nav.no>
2026-04-30 10:31:27 +0200

    midlertidig fiks for feilet melding

--- src/main/kotlin/kafka/kafka/DialogMeldingTolker.kt
+++ src/main/kotlin/kafka/kafka/DialogMeldingTolker.kt
@@ -20,9 +20,10 @@ class DialogMeldingTolker(
                 .getOrElse { e ->
                     logger.error("Klarte ikke deserialisere dialog-melding. Melding blir ikke prosessert.")
                     sikkerLogger.error("Klarte ikke deserialisere dialog-melding. Melding blir ikke prosessert. Melding: $melding", e)
+                    return
                 }
 
-        runCatching { dialogportenService.opprettDialogForFritakAgp(dekodMelding as DialogMelding) }
+        runCatching { dialogportenService.opprettDialogForFritakAgp(dekodMelding) }
             .getOrElse { e ->
                 logger.error("Klarte ikke sende dialog-melding til Dialogporten. Melding blir ikke prosessert.")
                 sikkerLogger.error("Klarte ikke sende dialog-melding til Dialogporten. Melding blir ikke prosessert. Melding: $melding", e)
