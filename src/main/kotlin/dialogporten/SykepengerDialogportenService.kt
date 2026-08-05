ff1b38e1dbb9b8f122d0e1808ca568f621f5d52f
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-08-05 14:31:58 +0200

    Legg til tolist

--- src/main/kotlin/dialogporten/SykepengerDialogportenService.kt
+++ src/main/kotlin/dialogporten/SykepengerDialogportenService.kt
@@ -103,7 +103,7 @@ class SykepengerDialogportenService(
 
                     val transmissions =
                         transaction {
-                            dialog.transmissions
+                            dialog.transmissions.toList()
                         }
 
                     transmissions
